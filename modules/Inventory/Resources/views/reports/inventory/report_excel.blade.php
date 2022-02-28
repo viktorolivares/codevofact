d06043de4fcef0446ab4f11ab8efdd41b83e87e0
viktorolivares <viktorolivares@gmail.com>
2022-02-28 12:31:52 -0500

    Actualizar

--- modules/Inventory/Resources/views/reports/inventory/report_excel.blade.php
+++ modules/Inventory/Resources/views/reports/inventory/report_excel.blade.php
@@ -80,9 +80,9 @@
                                     <td>{{$value->item->purchase_unit_price}}</td>
                                     <td>{{number_format($total_line, 6)}}</td>
                                     <td>{{$value->item->sale_unit_price}}</td>
-                                    <td>{{ $value->item->brand['name']}}</td>
-                                    <td>{{ $value->item->color['name'] }}</td>
-                                    <td>{{ $value->item->size['name'] }}</td>
+                                    <td>{{ isset($value->item->brand['name']) }}</td>
+                                    <td>{{ isset($value->item->color['name']) }}</td>
+                                    <td>{{ isset($value->item->size['name']) }}</td>
                                     <td>{{ $value->item->date_of_due }}</td>
                                     <td>{{$value->warehouse->description}}</td>
                                 </tr>
