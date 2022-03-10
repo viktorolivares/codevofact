4253178e0e3436cbc3764791d1fd71437e2beb05
viktorolivares <viktorolivares@gmail.com>
2022-03-10 13:07:57 -0500

    Update

--- modules/Inventory/Resources/views/reports/inventory/index.blade.php
+++ modules/Inventory/Resources/views/reports/inventory/index.blade.php
@@ -82,9 +82,9 @@
                                             <td class="celda">{{$value->stock}}</td>
                                             <td class="celda">{{$value->item->sale_unit_price}}</td>
                                             <td class="celda">{{$value->item->purchase_unit_price}}</td>
-                                            <td class="celda">{{ $value->item->brand['name'] }}</td>
-                                            <td class="celda">{{ $value->item->color['name'] }}</td>
-                                            <td class="celda">{{ $value->item->size['name'] }}</td>
+                                            <td class="celda">{{ isset($value->item->brand['name']) ? $value->item->brand['name'] : '' }}</td>
+                                            <td class="celda">{{ isset($value->item->color['name']) ? $value->item->color['name'] : ''  }}</td>
+                                            <td class="celda">{{ isset($value->item->size['name']) ? $value->item->size['name'] : '' }}</td>
                                             <td class="celda">{{ $value->item->date_of_due }}</td>
                                             <td class="celda">{{$value->warehouse->description}}</td>
                                         </tr>
